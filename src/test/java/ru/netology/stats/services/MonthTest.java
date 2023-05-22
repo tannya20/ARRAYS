0854f9e521cdd4142ce29134b329fc884f8c94a6
Таня Кишинская <lolo-20@yandex.ru>
2023-05-22 14:28:46 +0300

    sumMaxMin

--- src/test/java/ru/netology/stats/services/MonthTest.java
+++ src/test/java/ru/netology/stats/services/MonthTest.java
@@ -6,27 +6,44 @@ import org.junit.jupiter.api.Test;
 
 public class MonthTest {
 
-    @Test
-    public void StatsServiceMonth() {
-        StatsService service = new StatsService();
-
-        int[] Month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};   //набор данных о продажах конкретного предприятия по месяцам
-
-        int expectedMonth = 1;
-
-        Assertions.assertEquals(expectedMonth, StatsService.minSales(Month));
+    @Test //сумму всех продаж
+    public static void StatsServiceMonthSum() {
+        int[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int sum = 0;
+        for (int i : month) {
+            sum = sum + month[i];
+        }
     }
 
-//    @Test //сумму всех продаж
-//
-//    @Test //среднюю сумму продаж в месяц
-//
-//    @Test //номер месяца, в котором был пик продаж, то есть осуществлены продажи на максимальную сумму
-//
-//    @Test //номер месяца, в котором был минимум продаж, то есть осуществлены продажи на минимальную сумму
-//
-//    @Test //количество месяцев, в которых продажи были ниже среднего
-//
-//    @Test //количество месяцев, в которых продажи были выше среднего
+    @Test //среднюю сумму продаж в месяц
+    public static void StatsServiceMonthAverage() {
+        int[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int sum = 0;
+        for (int i : month) {
+            sum += i;
+        }
+    }
 
+    @Test //min
+    public static void StatsServiceMonthMin() {
+        int[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int min = month[0];
+        for (int i : month) {
+            if (i < min) {
+                min = i;
+            }
+        }
+    }
+
+
+    @Test //max
+    public static void StatsServiceMonthMax() {
+        int[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int max = month[0];
+        for (int i : month) {
+            if (i > max) {
+                max = i;
+            }
+        }
+    }
 }

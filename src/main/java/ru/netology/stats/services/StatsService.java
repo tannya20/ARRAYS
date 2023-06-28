7e7864eb863ce030c4c1c8e9ab6440717cbaeb8e
Таня Кишинская <lolo-20@yandex.ru>
2023-06-28 23:09:46 +0300

    help

--- src/main/java/ru/netology/stats/services/StatsService.java
+++ src/main/java/ru/netology/stats/services/StatsService.java
@@ -1,17 +1,54 @@
 package ru.netology.stats.services;
 
 
-public class StatsService {
-    //пример
-    public static int minSales(int[] sales) {
-        int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+public class StatsService {//Min
 
+    public static int minSales(int[] sales) {
+        int minMonth = 0;
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] <= sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
-                minMonth = i; // запомним его как минимальный
+            if (sales[i] <= sales[minMonth]) {
+                minMonth = i;
             }
         }
 
-        return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+        return minMonth + 1;
+    }
+
+    public static class StatsServiceMax {//Max
+        public static int minSales(int[] sales) {
+            int maxMonth = 0;
+            for (int i = 0; i > sales.length; i++) {
+                if (sales[i] >= sales[maxMonth]) {
+                    maxMonth = i;
+                }
+            }
+            return maxMonth + 1;
+        }
     }
 }
+public static class StatsServiceAverage {//Average
+
+    public static int averageSales(int[] sales) {
+        int averageMonth = 0;
+        for (int i = 0; i > sales.length; i++) {
+            if (sales[i] : sales[averageMonth]) {
+                averageMonth += i;
+            }
+        }
+        return averageMonth + 1;
+    }
+}
+            public class StatsServiceSum {//Sum
+
+                public static int SumSales(int[] sales) {
+                    int SumMonth = 0;
+                    for (int i = 0; i > sales.length;i++){
+                        int sumMonth = 0;
+                        if (sales[i] = sales[sumMonth]) {
+                            sumMonth = sumMonth +=[i];
+                        }
+                    }
+                }
+            }
+        }
+    }
